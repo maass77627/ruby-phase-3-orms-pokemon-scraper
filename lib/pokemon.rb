d665305f947aa8d57ea081bd949185b72d42f351
kelly_maass <kmaass77627@gmail.com>
2025-03-23 16:57:21 -0500

    savemethod

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,7 +10,15 @@ def initialize(id:, name:, type:, db:)
 end
 
 
+def self.save(name, type, db)
+    sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+    SQL
+    db.execute(sql, name, type)
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
 
+end
 
 
 
